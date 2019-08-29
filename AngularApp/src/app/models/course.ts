5db60f981e2889d5cf02e0cc6d93ad208279b896
edaibram <eda_ibram@yahoo.com>
2019-08-29 10:26:52 +0300

    eda d4.1

--- AngularApp/src/app/models/course.ts
+++
@@ -1,9 +0,0 @@
-export class Course {
-    id : number;
-    courseName : string;
-    capacity : number;
-    actualCapacity : number;
-    startDate : Date;
-    endDate : Date;
-    projectManager : string;
-}
