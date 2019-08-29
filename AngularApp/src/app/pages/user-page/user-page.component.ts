ae237d73a072d57726df072ca531b523c7eddf4e
unknown <alexbonteanu22@gmail.com>
2019-08-29 11:10:55 +0300

    deleted test-page

--- AngularApp/src/app/pages/user-page/user-page.component.ts
+++ AngularApp/src/app/pages/user-page/user-page.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit, Input } from '@angular/core';
+import { Component, OnInit} from '@angular/core';
 import { User } from 'src/app/models/user';
 import { Router } from '@angular/router';
 import { LoginPageComponent } from '../login-page/login-page.component';
